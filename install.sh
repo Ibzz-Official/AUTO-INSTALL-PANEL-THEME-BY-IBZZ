8b5715be6aeb07e2b30f3d27c5ae10748cae8eb7
IbzzXdzz <stockibzzstore@gmail.com>
2024-12-18 20:17:16 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -399,7 +399,7 @@ cd /var/www/pterodactyl || { echo "Direktori tidak ditemukan"; exit 1; }
 # Membuat lokasi baru
 php artisan p:user:make <<EOF
 yes
-hackback@gmail.com
+ibzzmodehb@gmail.com
 $user
 $user
 $user
